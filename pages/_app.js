d516fb3a49c557852135d32b1d52935fc373751d
Carlos-Nasciemnto <acntonycar62@gmail.com>
2021-12-28 06:52:42 -0300

    layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Layout from "../components/layout";
+import layout from "../components/layout";
 import '../css/styles.css';
 
 
 
-const MyApp = ({Component, pageProps}) => {
+const MyApp = ({components, pageProps}) => {
 
     return(
-        <Layout>
-           <Component {...pageProps} />
-        </Layout>
+        <layout>
+           <components {...pageProps} />
+        </layout>
     )
 }
 
